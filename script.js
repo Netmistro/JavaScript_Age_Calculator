a6bd24fabf62797f8f58a03d14cadcb064737688
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2024-01-02 21:10:50 -0400

    Final result of Age Calculator

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const btnE1 = document.getElementById("btnAge");
 const birthdayE1 = document.getElementById("birthday");
+const resultAge = document.getElementById("result");
 
 function calculateAge() {
   const birthdayValue = birthdayE1.value;
   if (birthdayValue === "") {
     alert("Please Enter Your Birthday");
   } else {
-const age=getAge(birthdayValue);
+    const age = getAge(birthdayValue);
+    resultAge.innerText = `Your Age is ${age} ${
+      age > 1 ? "years" : "year"
+    } Old`;
   }
 }
 
-function getAge(birthdayValue){
+function getAge(birthdayValue) {
   const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+  return age;
 }
 btnE1.addEventListener("click", calculateAge);
